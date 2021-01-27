b921c12447400e5656d486aeef30a53ac141f048
Harish Nataraj <harish.nataraj@anywhere.co>
2021-01-27 21:20:49 +0530

    naming conventions

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Team<FootballPlayer> fb = new Team<FootballPlayer>("Football");
-		Team<CricketPlayer> cric = new Team<CricketPlayer>("Cricket");
+		Team<FootballPlayer> footballTeam = new Team<FootballPlayer>("Football");
+		Team<CricketPlayer> cricketTeam = new Team<CricketPlayer>("Cricket");
 		
 		FootballPlayer ronaldo = new FootballPlayer("Christiano Ronaldo");
 		FootballPlayer griezman = new FootballPlayer("Antoin Greizman");
@@ -12,23 +12,23 @@ public class Main {
 		CricketPlayer dhoni = new CricketPlayer("MS Dhoni");
 		CricketPlayer dhoni1 = new CricketPlayer("MS Dhoni");
 		
-		fb.addPlayer(ronaldo);
- 		fb.addPlayer(griezman);
+		footballTeam.addPlayer(ronaldo);
+		footballTeam.addPlayer(griezman);
 		
-		cric.addPlayer(kohli);
-		cric.addPlayer(dhoni);
-		cric.addPlayer(dhoni1);
+		cricketTeam.addPlayer(kohli);
+		cricketTeam.addPlayer(dhoni);
+		cricketTeam.addPlayer(dhoni1);
 		System.out.println("==================================================");
 		
 		System.out.println("Football players :");
-		for(FootballPlayer player : fb.getPlayers()) {
+		for(FootballPlayer player : footballTeam.getPlayers()) {
 			System.out.println(player.getPlayerName());
 		}
 		
 		System.out.println("==================================================");
 		
 		System.out.println("Cricket players :");
-		for(CricketPlayer player : cric.getPlayers()) {
+		for(CricketPlayer player : cricketTeam.getPlayers()) {
 			System.out.println(player.getPlayerName());
 		}
 
